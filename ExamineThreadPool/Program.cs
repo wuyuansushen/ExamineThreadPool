28aaf26aedcc751bbbed1321f3a5cb4faca83a68
wuyuansushen <wuyuansushen@gmail.com>
2021-11-08 20:36:29 +0800

    Min and Max

--- ExamineThreadPool/Program.cs
+++ ExamineThreadPool/Program.cs
@@ -8,11 +8,17 @@ namespace ExamineThreadPool
         static void Main(string[] args)
         {
             int maxWorkerThread, maxIOAsyncThread;
+            int minWorkerThread, minIOAsyncThread;
             ThreadPool.GetMaxThreads(out maxWorkerThread, out maxIOAsyncThread);
+            ThreadPool.GetMinThreads(out minWorkerThread, out minIOAsyncThread);
             Console.WriteLine($"This computer Processor is {Environment.ProcessorCount}\n");
-            Console.WriteLine($"Examine Thread pool...\n\n" +
-                $"Max worker Thread: {maxWorkerThread}\n" +
-                $"Max async I/O Thread: {maxIOAsyncThread}");
+            Console.WriteLine($"Examine Thread pool...\n");
+            Console.WriteLine(
+                $"Min worker Thread: {minWorkerThread}\n" +
+                $"Max worker Thread: {maxWorkerThread}\n");
+            Console.WriteLine(
+                $"Min async I/O Thread: {minIOAsyncThread}\n"+
+                $"Max async I/O Thread: {maxIOAsyncThread}\n");
         }
     }
 }
