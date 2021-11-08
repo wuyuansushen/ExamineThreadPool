7406bbfc78aa84150762a3792eff6be74dd42421
wuyuansushen <wuyuansushen@gmail.com>
2021-11-08 20:30:46 +0800

    Add project files.

---
+++ ExamineThreadPool/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace ExamineThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxWorkerThread, maxIOAsyncThread;
+            ThreadPool.GetMaxThreads(out maxWorkerThread, out maxIOAsyncThread);
+            Console.WriteLine($"This computer Processor is {Environment.ProcessorCount}\n");
+            Console.WriteLine($"Examine Thread pool...\n\n" +
+                $"Max worker Thread: {maxWorkerThread}\n" +
+                $"Max async I/O Thread: {maxIOAsyncThread}");
+        }
+    }
+}
